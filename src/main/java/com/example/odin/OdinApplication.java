2f9bfcd5c609adcd5445968330ae73483d4649f2
rausss <wolf_rosemarie@hotmal.com>
2022-09-06 15:49:09 +0200

    first

---
+++ src/main/java/com/example/odin/OdinApplication.java
@@ -0,0 +1,13 @@
+package com.example.odin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OdinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OdinApplication.class, args);
+	}
+
+}
